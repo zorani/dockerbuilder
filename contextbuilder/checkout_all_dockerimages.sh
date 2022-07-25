8ea79749e515431fdb4bfeff2a4d48217f314e26
zorani <zorani@gmail.com>
2022-07-25 17:34:35 +0200

    save

--- contextbuilder/checkout_all_dockerimages.sh
+++ contextbuilder/checkout_all_dockerimages.sh
@@ -6,6 +6,8 @@ declare -A dockergitrepos
 dockergitrepos["dockerimage_ubuntu22.04_base"]="https://github.com/zorani/dockerimage_ubuntu22.04_base.git"
 dockergitrepos["dockerimage_ubuntu22.04_add_default_user"]="https://github.com/zorani/dockerimage_ubuntu22.04_add_default_user.git"
 dockergitrepos["dockerimage_ubuntu22.04_pythondev"]="https://github.com/zorani/dockerimage_ubuntu22.04_pythondev.git"
+dockergitrepos["dockerimage_ubuntu22.04_python"]="https://github.com/zorani/dockerimage_ubuntu22.04_python.git"
+dockergitrepos["dockerimage_ubuntu22.04_add_secrets"]="https://github.com/zorani/dockerimage_ubuntu22.04_base.git"
 
 for key in ${!dockergitrepos[@]}; do 
     echo ${key}
@@ -19,7 +21,7 @@ done
 
 #Lets delete all the repos if you dont need them.
 #Comment/Uncomment next line if you want to delete/not-delete
-#exit 9999
+##exit 9999
 
 for key in ${!dockergitrepos[@]}; do 
     if [ ${key} ]
