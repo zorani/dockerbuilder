c6587ba8ca6d649f335c2c9aa29aecdb88ab16f7
zorani <zorani@gmail.com>
2022-08-19 20:20:45 +0200

    save

---
+++ contextbuilder/delete_all_checkedout_git_images.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+cd .. 
+
+declare -A dockergitrepos
+dockergitrepos["dockerimage_ubuntu22.04_base"]="https://github.com/zorani/dockerimage_ubuntu22.04_base.git"
+dockergitrepos["dockerimage_ubuntu22.04_add_default_user"]="https://github.com/zorani/dockerimage_ubuntu22.04_add_default_user.git"
+dockergitrepos["dockerimage_ubuntu22.04_pythondev"]="https://github.com/zorani/dockerimage_ubuntu22.04_pythondev.git"
+dockergitrepos["dockerimage_ubuntu22.04_python"]="https://github.com/zorani/dockerimage_ubuntu22.04_python.git"
+dockergitrepos["dockerimage_ubuntu22.04_add_secrets"]="https://github.com/zorani/dockerimage_ubuntu22.04_add_secrets.git"
+
+#for key in ${!dockergitrepos[@]}; do 
+#    echo ${key}
+#    if [ ! -d ${key} ]
+#    then 
+#        git clone ${dockergitrepos[${key}]}
+#    else
+#        echo "git hub repo \"${key}\" already checked out"
+#    fi 
+#done
+
+#Lets delete all the repos if you dont need them.
+#Comment/Uncomment next line if you want to delete/not-delete
+#exit 9999
+
+for key in ${!dockergitrepos[@]}; do 
+    if [ ${key} ]
+    then
+        echo "deleting ${key} ..." 
+        rm -rf ${key}
+    fi 
+done
+
